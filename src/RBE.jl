b7e05876cdde0b5f63a7d18c3eb37621f6adc1df
shiyibai5315 <sbai313@connect.hkust-gz.edu.cn>
2024-06-03 11:31:12 +0800

    update

--- src/RBE.jl
+++ src/RBE.jl
@@ -9,7 +9,7 @@ using StatsBase
 
 include("math_functions.jl")
 include("mh_sampler.jl")
-include("../test/test_RBE.jl")
+include("../test/run_tests.jl")
 
 export calculate_H, calculate_S, calculate_probability, mh_sample, calculate_force, calculate_Fi, generate_k_vector, sampling, update_rho_k, calculate_G, calculate_Fi_short
 export RBEInteraction
